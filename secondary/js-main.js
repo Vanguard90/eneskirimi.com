a3aeb7afcdad15e9f7cde083482dfe1190d225c4
Vanguard90 <enesmil@gmail.com>
2016-10-31 19:37:34 +0100

    Better and self-written JS for buttons

--- secondary/js-main.js
+++ secondary/js-main.js
@@ -3,6 +3,14 @@ function alertBox () {
 	alert("Clicked");
 }
 
+/*
 var xyz = document.getElementsByClassName("abcd");
 var xyz = xyz[0];
 xyz.addEventListener("click", function alertBox() {alert('This is supposed to work like this.')}, false);
+*/
+
+var buttonClick = document.getElementsByClassName("abcd");
+var i;
+for (i = 0; i < buttonClick.length; i++) {
+    buttonClick[i].addEventListener("click", function alertBox() {alert('This is supposed to work like this.')}, false);
+}
