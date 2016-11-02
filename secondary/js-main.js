9167179ead1d2af8ca567de5e5d4eed0dd9756f9
Vanguard90 <enesmil@gmail.com>
2016-11-02 20:37:15 +0100

    Teaser area now reorganizes itself and looks consistent

--- secondary/js-main.js
+++ secondary/js-main.js
@@ -5,8 +5,29 @@ var xyz = xyz[0];
 xyz.addEventListener("click", function alertBox() {alert('This is supposed to work like this.')}, false);
 */
 
+/* Button click start */
+
 var buttonClick = document.getElementsByClassName("abcd");
 var i;
 for (i = 0; i < buttonClick.length; i++) {
-    buttonClick[i].addEventListener("click", function() {alert('This is supposed to work like this.')}, false);
+    buttonClick[i].addEventListener("click", function clickFunction() {alert('This is supposed to work like this.')}, false);
 }
+
+/* Button click end */
+
+var windowWidth = parseInt(window.innerWidth);
+
+ if (windowWidth <= 700) {
+
+var mobileTeaser = document.getElementById("mobile-teaser");
+
+var mobileTeaserImage = document.getElementById("mobile-teaser-image");
+
+mobileTeaser.insertBefore(mobileTeaserImage, mobileTeaser.childNodes[0]);
+
+}
+
+
+	/*         var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
+        */
