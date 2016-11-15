46c94faddfbf044924eab676bc9c459f8b7fe089
Vanguard90 <enesmil@gmail.com>
2016-11-15 19:40:08 +0100

    Scrapped CSS header navigations. Added javascript instead. Null element error that needs to be fixed.

--- secondary/js-main.js
+++ secondary/js-main.js
@@ -19,6 +19,46 @@ mobileTeaser.insertBefore(mobileTeaserImage, mobileTeaser.childNodes[0]);
 
 }
 
+/* setInterval(function, milliseconds) */
+
+function leaf1 () {
+
+	var secondImage = document.getElementsByClassName("second-image");
+	var headerDiv = document.getElementById("header-div");
+	var thirdImage = document.getElementsByClassName("third-image");
+
+	headerDiv.removeChild(secondImage);
+	headerDiv.removeChild(thirdImage);
+
+}
+
+function leaf2 () {
+
+	var firstImage = document.getElementsByClassName("first-image");
+	var headerDiv = document.getElementById("header-div");
+	var thirdImage = document.getElementsByClassName("third-image");
+
+	headerDiv.removeChild(firstImage);
+	headerDiv.removeChild(thirdImage);
+
+}
+
+function leaf3 () {
+
+	var firstImage = document.getElementsByClassName("first-image");
+	var headerDiv = document.getElementById("header-div");
+	var thirdImage = document.getElementsByClassName("third-image");
+
+	headerDiv.removeChild(firstImage);
+	headerDiv.removeChild(secondImage);
+
+}
+
+setInterval(leaf1, 3000);
+setInterval(leaf2, 6000);
+setInterval(leaf3, 9000);
+
+
  /*
 let mouseover = document.getElementsByClassName("header-element");
 for (i = 0; i < mouseover.length; i++ ) {
