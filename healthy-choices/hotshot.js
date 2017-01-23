8c53fa0561d6f33f1e67d394aa3041cde981e122
Vanguard90 <enesmil@gmail.com>
2017-01-23 23:06:40 +0100

    Not much progress on scrollable firing events so far. Looking for solutions.

--- healthy-choices/hotshot.js
+++ healthy-choices/hotshot.js
@@ -1,9 +1,27 @@
-const usTitle = document.querySelector(".stat-section");
+const html = document.querySelector("html");
+const usTitle = document.querySelector(".h2-title");
+const h2 = document.querySelector("h2");
 
-function topSlide () {
+function isScrolledIntoView(el) {
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
 
-	
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
+
+function usTitleCheck(usTitle) {
+
+	if (document.documentElement.scrollTop < 1000 && document.documentElement.scrollTop > 50) {
+	if (usTitle.classList.contains("active")) {
+		;
+	} else {
+		usTitle.classList.add("active");
+	}
+} else {
+	usTitle.classList.remove("active");
+}
 
 }
 
-usTitle.addEventListener("scroll", topSlide);
+setInterval(usTitleCheck(usTitle), 1000);
