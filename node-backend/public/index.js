8c547025a3d307ec5d0f0b42f6c9392c3ec5e4a9
Vanguard90 <enesmil@gmail.com>
2018-12-03 00:23:27 +0100

    Commiting what's on the local to git

---
+++ node-backend/public/index.js
@@ -0,0 +1,7 @@
+import style from './scss/style.scss';
+import masthead from './img/masthead.jpg';
+
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
