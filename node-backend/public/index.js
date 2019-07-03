b6759942e1c7c55cb2022d6919f21b2c334d5bfd
Vanguard90 <enesmil@gmail.com>
2019-07-03 22:34:47 +0200

    Removed node-backend folder since I ended up using nginx as a web server and not node.

--- node-backend/public/index.js
+++
@@ -1,7 +0,0 @@
-import style from './scss/style.scss';
-import masthead from './img/masthead.jpg';
-
-window.addEventListener('resize', function () { 
-    "use strict";
-    window.location.reload(); 
-});
