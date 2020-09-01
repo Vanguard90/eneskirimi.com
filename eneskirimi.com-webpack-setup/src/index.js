bceee0c78b1a2d008a3c857f0a9840aea3a32a24
Vanguard90 <enesmil@gmail.com>
2020-09-01 18:13:57 +0200

    Years of experience are now set dynmacially but in a crude way

--- eneskirimi.com-webpack-setup/src/index.js
+++ eneskirimi.com-webpack-setup/src/index.js
@@ -1,7 +1,20 @@
 import style from './scss/style.scss';
 import masthead from './img/masthead.jpg';
 
-window.addEventListener('resize', function () { 
+window.addEventListener('resize', function () {
     "use strict";
-    window.location.reload(); 
+    window.location.reload();
 });
+
+document.addEventListener('DOMContentLoaded', yearSetter(), false);
+
+/**
+ * Dynamically update the years of experience in a crude way
+ */
+function yearSetter() {
+    const startingYear = 2016 - 1; // Start counting from the start of 2016, simple trick
+    const currentYear = new Date().getFullYear();
+
+    const yearsAmount = document.querySelector('#years-amount');
+    yearsAmount.textContent = currentYear - startingYear;
+}
