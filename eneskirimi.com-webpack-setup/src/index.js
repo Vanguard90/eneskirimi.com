93ecbc29127a5d30ab4bf467082f06b1a67c1142
Vanguard90 <enesmil@gmail.com>
2021-06-06 00:22:16 +0200

    Removed unused masthead import

--- eneskirimi.com-webpack-setup/src/index.js
+++ eneskirimi.com-webpack-setup/src/index.js
@@ -1,20 +1,19 @@
-import style from './scss/style.scss';
-import masthead from './img/masthead.jpg';
+import style from "./scss/style.scss";
 
-window.addEventListener('resize', function () {
-    "use strict";
-    window.location.reload();
+window.addEventListener("resize", function () {
+  "use strict";
+  window.location.reload();
 });
 
-document.addEventListener('DOMContentLoaded', yearSetter(), false);
+document.addEventListener("DOMContentLoaded", yearSetter(), false);
 
 /**
  * Dynamically update the years of experience in a crude way
  */
 function yearSetter() {
-    const startingYear = 2016 - 1; // Start counting from the start of 2016, simple trick
-    const currentYear = new Date().getFullYear();
+  const startingYear = 2016 - 1; // Start counting from the start of 2016, simple trick
+  const currentYear = new Date().getFullYear();
 
-    const yearsAmount = document.querySelector('#years-amount');
-    yearsAmount.textContent = currentYear - startingYear;
+  const yearsAmount = document.querySelector("#years-amount");
+  yearsAmount.textContent = currentYear - startingYear;
 }
