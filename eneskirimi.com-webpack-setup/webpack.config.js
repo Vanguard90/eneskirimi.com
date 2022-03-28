1b44ade078317434e84779355734c9e444e9ce5a
Vanguard90 <enesmil@gmail.com>
2022-03-29 00:35:53 +0200

    Updated file-loader as an interim solution before webpack 5 migration

--- eneskirimi.com-webpack-setup/webpack.config.js
+++ eneskirimi.com-webpack-setup/webpack.config.js
@@ -33,11 +33,21 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false,
+          },
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false,
+          },
+        },
       },
     ],
   },
